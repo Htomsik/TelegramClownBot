2dc92bc45102998a380b04f998849882957b79ba
Konstantin <wont.miner@gmail.com>
2025-07-24 00:07:24 +0300

    Fetch: Add sticker reaction on message

--- TelegramClownBot/Services/Interfaces/IMessageService.cs
+++ TelegramClownBot/Services/Interfaces/IMessageService.cs
@@ -4,7 +4,24 @@ namespace TelegramClownBot.Services.Interfaces
 {
     public interface IMessageService
     {
-        Task SendMessageAsync(long userId, string message);
+        /// <summary>
+        ///     Send text message to user
+        /// </summary>
+        Task SendMessageAsync(User targetUser, string message);
+
+        /// <summary>
+        ///     Send sticker to user (NOT EMOJI)
+        /// </summary>
+        Task SendStickerMessageAsync(User targetUser, string stickerSetName, string stickerEmotion);
+        
+        /// <summary>
+        ///     Send emoji reaction to message
+        /// </summary>
+        Task SendMessageReactionAsync(int messageId, string reactionEmotion);
+        
+        /// <summary>
+        ///     Background message worker
+        /// </summary>
         Task ProcessUpdatesAsync(UpdatesBase updates);
     }
 } 
--- TelegramClownBot/Services/MessageService.cs
+++ TelegramClownBot/Services/MessageService.cs
@@ -1,8 +1,10 @@
 using System.Linq;
 using System.Threading.Tasks;
 using TelegramClownBot.Core;
+using TelegramClownBot.Models;
 using TelegramClownBot.Services.Interfaces;
 using TL;
+using WTelegram;
 
 namespace TelegramClownBot.Services
 {
@@ -15,16 +17,57 @@ namespace TelegramClownBot.Services
             _context = context;
         }
         
-        public async Task SendMessageAsync(long userId, string message)
+        public async Task SendMessageAsync(User?  targetUser, string message)
         {
-            var dialogs = await _context.TelegramClient.Messages_GetAllDialogs();
-            
-            if(!dialogs.users.TryGetValue(userId, out User? target))
+            if (_context.TelegramClient is null 
+                || targetUser is null  
+                || targetUser.ID == 0)
                 return;
             
-            await _context.TelegramClient.SendMessageAsync(target, message);
+            await _context.TelegramClient.SendMessageAsync(targetUser, message);
         }
         
+        public async Task SendStickerMessageAsync(User? targetUser, string stickerSetName, string stickerEmotion)
+        {
+            if (_context.TelegramClient is null 
+                || targetUser is null  
+                || targetUser.ID == 0)
+                return;
+            
+            // TG have strange sticker API
+            // Pack combine of two arrays 
+            // One array with dock (body of sticker) + id (mainArray)
+            // Second array with emotionName + id (idArray)
+            
+            // Get needed sticker idArray
+            var stickerSets = await _context.TelegramClient.Messages_GetStickerSet(stickerSetName);
+
+            // Find needed sticker in idArray
+            var sticker = stickerSets.packs.FirstOrDefault(sticker => sticker.emoticon == stickerEmotion);
+            if (sticker == null || sticker.documents.Length == 0)
+                return;
+            
+            // Get sticker Id from idArray
+            var stickerId = stickerSets.packs.First(p => p.emoticon == stickerEmotion).documents[0];
+            
+            // Get dock by id from mainArray
+            var stickerDock = stickerSets.documents.First(d => d.ID == stickerId);
+            if (stickerDock == null)
+                return;
+            
+            await _context.TelegramClient.SendMessageAsync(targetUser, null, stickerDock);
+        }
+
+        public async Task SendMessageReactionAsync(int messageId, string reactionEmotion)
+        {
+            await _context.TelegramClient.Messages_SendReaction(
+                peer: InputPeer.Self,
+                msg_id: messageId,
+                reaction: new Reaction[] { new ReactionEmoji { emoticon = reactionEmotion } }
+            );
+        }
+
+
         public async Task ProcessUpdatesAsync(UpdatesBase updates)
         {
             if (updates.UpdateList == null)
@@ -41,24 +84,21 @@ namespace TelegramClownBot.Services
         
         private async Task ProcessMessageAsync(UpdateNewMessage unm)
         {
-            var message = unm.message;
-            
-            if (message.From == null)
+            if (unm.message.From == null)
                 return;
             
             // Find user in our list
-            var user = _context.Users.FirstOrDefault(u => u.Id == message.From.ID);
+            var user = _context.Users.FirstOrDefault(u => u.Id == unm.message.From.ID);
             
             // Check if the user is selected in the selection service
-            if (user == null || !_context.ClownSelectionService.IsSelected(user, userSelect => user.Id == userSelect.Id))
-                    return;
+            if (user == null ||
+                !_context.ClownSelectionService.IsSelected(user, userSelect => user.Id == userSelect.Id))
+                return;
             
-            // Send clown reaction
-            await _context.TelegramClient.Messages_SendReaction(
-                peer: InputPeer.Self,
-                msg_id: message.ID,
-                reaction: new[] { new ReactionEmoji { emoticon = "🤡" } }
-            );
+            var stickerTask = SendStickerMessageAsync(user.OriginalUser, "Chomyske", "🍜");
+            var reactionTask = SendMessageReactionAsync(unm.message.ID, "🤡");
+            
+            await Task.WhenAll(stickerTask, reactionTask);
         }
     }
 } 
